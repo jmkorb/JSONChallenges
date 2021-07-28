7e2bdf726a25b104ec4523dfa9d89ed445cdda7b
Jacob Korb <jacob.korb@gmail.com>
2021-07-27 22:10:42 -0400

    json challenges

---
+++ JSONChallenge/Bonus/Order.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONChallenge.Bonus
+{
+    public class Address
+    {
+        public string AddressLine1 { get; set; }
+        public string AddressLine2 { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int Zip { get; set; }
+    }
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public Address ShippingAddress { get; set; }
+    }
+    public class Item 
+    {
+        public string Sku { get; set; }
+        public int Quantity { get; set; }
+        public double UnitPrice { get; set; }
+        public double TotalPrice { get; set; }
+    }
+    public class Order
+    {
+        public string OrderID { get; set; }
+
+        public Customer Customer { get; set; }
+        public List<Item> LineItems { get; set; }
+        public DateTime PurchaseDate { get; set; }
+    }
+}
---
+++ JSONChallenge/Challenge2/Security.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JSONChallenge.Challenge2
+{
+    public class Security
+    {
+        public int BadgeID { get; set; }
+        public List<string> ListOfDoors { get; set; }
+        public int MyProperty { get; set; }
+        public bool HasSecurityClearance { get; set; }
+        public string EmployeeID { get; set; }
+        public DateTime CreatedOn { get; set; }
+    }
+}
